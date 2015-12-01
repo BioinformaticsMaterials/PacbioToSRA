696c121e259414e3ce4442c379d3b346f106c1dd
Chris Debug <chrisdebug@gmail.com>
2015-11-30 16:25:33 -0800

    Adds tests for hash/md5 utils.

---
+++ tests/PacbioToSRA/test_hash_utils.py
@@ -0,0 +1,19 @@
+import unittest
+
+from PacbioToSRA.hash_utils import *
+from tests import *
+
+
+class TestHashUtils(unittest.TestCase):
+    """To run these tests:
+    $ python -m unittest -v tests.PacbioToSRA.test_hash_utils
+    $ python -m unittest -v tests.PacbioToSRA.test_hash_utils.TestHashUtils
+    $ python -m unittest -v tests.PacbioToSRA.test_hash_utils.TestHashUtils.test_md5
+    """
+
+    def test_md5(self):
+        self.assertEqual(
+            '8300a100d55f896080e0c4f18895414e',     # calculated on the command line
+            md5(MD5SUM_TEST_FILE)
+        )
+
--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ from os.path import realpath, dirname, join
 TEST_ROOT_FULLPATH = dirname(realpath(__file__))
 CELL_ANALYSIS_RESULT_WITH_H5_FILES_TEST_DATA_PATH = join(TEST_ROOT_FULLPATH, 'data', 'cell_analysis_result_with_h5')
 CELL_ANALYSIS_RESULT_WITH_BAM_FILES_TEST_DATA_PATH = join(TEST_ROOT_FULLPATH, 'data', 'cell_analysis_result_with_bam')
-
+MD5SUM_TEST_FILE = join(TEST_ROOT_FULLPATH, 'data', 'md5sum.txt')
