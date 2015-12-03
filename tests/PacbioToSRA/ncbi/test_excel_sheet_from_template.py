87fd2dd0afaab622ea7f1250278a4ef824e79deb
Chris Debug <chrisdebug@gmail.com>
2015-12-03 14:15:54 -0800

    Adds test for generating the NCBI excel sheet.

---
+++ tests/PacbioToSRA/ncbi/test_excel_sheet_from_template.py
@@ -0,0 +1,20 @@
+import unittest
+
+from PacbioToSRA.ncbi.excel_sheet_from_template import *
+from tests import *
+
+
+class TestExcelSheetFromTemplate(unittest.TestCase):
+    """To run these tests:
+    $ python -m unittest -v tests.PacbioToSRA.ncbi.test_excel_sheet_from_template
+    $ python -m unittest -v tests.PacbioToSRA.ncbi.test_excel_sheet_from_template.TestExcelSheetFromTemplate
+    $ python -m unittest -v tests.PacbioToSRA.ncbi.test_excel_sheet_from_template.TestExcelSheetFromTemplate.test_that_excel_file_already_exists
+    """
+
+    def test_that_excel_file_already_exists(self):
+        with self.assertRaises(Exception):
+            ExcelSheetFromTemplate(__file__)
+
+
+if __name__ == '__main__':
+    unittest.main()
