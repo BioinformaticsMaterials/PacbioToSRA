97fcaa33747da64630f28fdc7568298de33d0cb5
Chris Debug <chrisdebug@gmail.com>
2016-02-22 14:53:53 -0800

    Fixes translator class renaming lines.

--- PacbioToSRA/ncbi/cell_analysis_bam_format_translator.py
+++ PacbioToSRA/ncbi/cell_analysis_bam_format_translator.py
@@ -7,7 +7,7 @@ from PacbioToSRA.ncbi.abstract_cell_analysis_translator import AbstractCellAnaly
 logger = logging.getLogger(__name__)
 
 
-class BamFormatCellAnalysisTranslator(AbstractCellAnalysisTranslator):
+class CellAnalysisBamFormatTranslator(AbstractCellAnalysisTranslator):
     @property
     def cell_analysis_format_class(self):
         return BamFormat
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     from PacbioToSRA.cell_analysis.bam_format import BamFormat
 
     ca_in_bam = BamFormat('/Users/clongboy/projects/PacbioToSRA/src/PacbioToSRA/temp/bam2/r54016_20160209_005307/1_A01')
-    bfcat = BamFormatCellAnalysisTranslator()
+    bfcat = CellAnalysisBamFormatTranslator()
     print bfcat.translate_sample_name(ca_in_bam)
     print bfcat.translate_library_id(ca_in_bam)
     print bfcat.translate_platform(ca_in_bam)
--- PacbioToSRA/ncbi/cell_analysis_hd5_format_translator.py
+++ PacbioToSRA/ncbi/cell_analysis_hd5_format_translator.py
@@ -7,7 +7,7 @@ from PacbioToSRA.ncbi.abstract_cell_analysis_translator import AbstractCellAnaly
 logger = logging.getLogger(__name__)
 
 
-class HD5FormatCellAnalysisTranslator(AbstractCellAnalysisTranslator):
+class CellAnalysisHD5FormatTranslator(AbstractCellAnalysisTranslator):
     @property
     def cell_analysis_format_class(self):
         return HD5Format
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     from PacbioToSRA.cell_analysis.hd5_format import HD5Format
 
     ca_in_hd5 = HD5Format('/Users/clongboy/projects/PacbioToSRA/src/PacbioToSRA/temp/2015-04-16_42141_1139__Hummingbird_17kb_plate_7/H01_1')
-    hd5cat = HD5FormatCellAnalysisTranslator()
+    hd5cat = CellAnalysisHD5FormatTranslator()
     print hd5cat.translate_sample_name(ca_in_hd5)
     print hd5cat.translate_library_id(ca_in_hd5)
     print hd5cat.translate_platform(ca_in_hd5)
