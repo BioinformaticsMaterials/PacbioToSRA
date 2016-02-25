eefffeebc600707b43db4e8a7efa5bb3d00965cf
Chris Debug <chrisdebug@gmail.com>
2016-02-25 14:30:33 -0800

    Adds test for ncbi's cell analysis translator classes.

---
+++ tests/PacbioToSRA/ncbi/test_abstract_cell_analysis_translator.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+
+from PacbioToSRA.ncbi.abstract_cell_analysis_translator import AbstractCellAnalysisTranslator
+from tests import *
+
+
+class TestAbstractFormat(unittest.TestCase):
+    """To run these tests:
+    $ python -m unittest -v tests.PacbioToSRA.cell_analysis.test_abstract_format
+    $ python -m unittest -v tests.PacbioToSRA.cell_analysis.test_abstract_formatTest.AbstractFormat
+    $ python -m unittest -v tests.PacbioToSRA.cell_analysis.test_abstract_formatTest.AbstractFormat.test__init__directory_does_not_exist
+
+    To run all test:
+    $ python -m unittest discover -v
+    """
+
+    def test_generate_run_id_from_filename(self):
+        test_cases = [
+            {
+                'input': 'this_is_the_run_id.txt',
+                'expected_result': 'this_is_the_run_id',
+            },
+            {
+                'input': 'this_is_the_run_id.1.2.3.txt',
+                'expected_result': 'this_is_the_run_id',
+            },
+            {
+                'input': '/the/full/path/to/this/file.log',
+                'expected_result': 'file',
+            },
+
+        ]
+
+        for test in test_cases:
+            self.assertEqual(
+                test['expected_result'],
+                AbstractCellAnalysisTranslator._generate_run_id_from_filename(test['input'])
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
