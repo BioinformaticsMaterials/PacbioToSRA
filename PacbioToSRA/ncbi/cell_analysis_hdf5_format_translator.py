5d6721df38c6bd6cb650a39ce93e1e8c97f81f6f
Chris Debug <chrisdebug@gmail.com>
2016-02-24 16:45:42 -0800

    Fixes hadoop format abbreviation references.

--- PacbioToSRA/ncbi/cell_analysis_hdf5_format_translator.py
+++ PacbioToSRA/ncbi/cell_analysis_hdf5_format_translator.py
@@ -1,6 +1,6 @@
 import logging
 
-from PacbioToSRA.cell_analysis.hd5_format import HD5Format
+from PacbioToSRA.cell_analysis.hdf5_format import HD5Format
 from PacbioToSRA.ncbi.abstract_cell_analysis_translator import AbstractCellAnalysisTranslator
 
 
@@ -32,7 +32,7 @@ class CellAnalysisHD5FormatTranslator(AbstractCellAnalysisTranslator):
 
 
 if __name__ == '__main__':
-    from PacbioToSRA.cell_analysis.hd5_format import HD5Format
+    from PacbioToSRA.cell_analysis.hdf5_format import HD5Format
 
     ca_in_hd5 = HD5Format('/Users/clongboy/projects/PacbioToSRA/src/PacbioToSRA/temp/2015-04-16_42141_1139__Hummingbird_17kb_plate_7/H01_1')
     hd5cat = CellAnalysisHD5FormatTranslator()
